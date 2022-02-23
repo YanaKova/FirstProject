33850f2eaf274be595b895a6098e6bac90188fc0
YanaKova <yanaakova@yahoo.com>
2022-02-23 18:51:17 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Poexali");
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+//Contr Alt L - put everething in odrer
+        var Tosya = 2022;
+        var Vlad = "30years";
+        var Yana = "27years";
+        System.out.println( Tosya + Vlad + Yana );
+
+    }
+
+}
+
